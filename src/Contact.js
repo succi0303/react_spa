2dd954554459d157eaa705b5b282137667b142d5
succi0303 <succi0303@gmail.com>
2019-07-09 10:25:36 +0900

    Initial commit

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTION</h2>
+        <p>
+          The easiest thing to do is post on our{" "}
+          <a href="http://forum.kirupa.com">forums</a>.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
