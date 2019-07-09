5399f574064bae01c375e63136476edbd7a67645
werckerbot <pleasemailus@wercker.com>
2019-07-09 01:32:59 +0000

    deploy from Takashi

---
+++ precache-manifest.c78601958aea8d8e437e1e7e4310c8e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823b102df67c14437e730a92418519cb",
+    "url": "/react_spa/index.html"
+  },
+  {
+    "revision": "5675688d1387d227aaf4",
+    "url": "/react_spa/static/css/main.f2fe7a5c.chunk.css"
+  },
+  {
+    "revision": "9403f0062c9f36dfc3ec",
+    "url": "/react_spa/static/js/2.6e08da32.chunk.js"
+  },
+  {
+    "revision": "5675688d1387d227aaf4",
+    "url": "/react_spa/static/js/main.543c8ed3.chunk.js"
+  },
+  {
+    "revision": "848c3f658688f69b3928",
+    "url": "/react_spa/static/js/runtime~main.fb24aae3.js"
+  }
+]);
